fe1a3e85acbd8421c91c848a707398e1319bdd25
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-09-10 23:32:24 +0530

    restartt

--- my-project/components/Header.js
+++ my-project/components/Header.js
@@ -1,5 +1,6 @@
 
-import Image from "next/image"
+import Image from "next/image" 
+
 function Header(){
     return(
         <div>
--- my-project/next.config.js
+++ my-project/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     images: {
-        domain: [
+        domains: [
             "links.papareact.com"
         ]
     }
