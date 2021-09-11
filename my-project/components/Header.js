20db4a588b20c228c15c3de54fc0f20f13f2e10d
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-09-11 23:37:48 +0530

    import heroicons

--- my-project/components/Header.js
+++ my-project/components/Header.js
@@ -1,5 +1,20 @@
 
-import Image from "next/image" 
+import Image from "next/image";
+import {
+    BellIcon,
+    ChatIcon,
+    ChevronDownIcon,
+    HomeIcon,
+    UserGroupIcon,
+    ViewGridAddIcon,
+} from "@heroicons/react/solid";
+import {
+    FlagIcon,
+    PlayIcon,
+    ShoppingCartIcon,
+    SearchIcon
+
+} from "@heroicons/react/outline"
 
 function Header(){
     return(
@@ -12,8 +27,13 @@ function Header(){
                height={40}
                layout="fixed"
                />
+
+               <div>
+
+               </div>
            </div>
            {/* Center */}
+
            {/* Right */}
 
         </div>
