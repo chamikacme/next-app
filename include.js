45b781ec5d47d8ab3c301dbd4cd20ddbad184182
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2022-10-13 23:37:09 +0530

    Create include.js

---
+++ include.js
@@ -0,0 +1,12 @@
+const array1 = ["a", "b", "c"];
+
+console.log(array1.includes("b"));
+// expected output: true
+
+const pets = ['cat', 'dog', 'bat'];
+
+console.log(pets.includes('cat'));
+// expected output: true
+
+console.log(pets.includes('at'));
+// expected output: false
